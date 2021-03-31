15482ef9e1d0b5b75b58ad0f972ca5a745f20db9
Connex Mac Book <pra_witra_2045@hotmail.com>
2021-03-31 10:23:09 +0700

    add Project

---
+++ MyTrainApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTrainApp
+//
+//  Created by Connex Mac Book on 23/3/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
